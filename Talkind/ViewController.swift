71750fef7961a40558abf0697076f99d89724053
azimgunes <azimgunesofficial@gmail.com>
2025-05-21 20:28:10 +0300

    Initial Commit

---
+++ Talkind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talkind
+//
+//  Created by Azim Güneş on 21.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
