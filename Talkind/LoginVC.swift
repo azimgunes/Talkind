4121c49e85240d5d9baa196c3abf7a8a9feb3734
azimgunes <azimgunesofficial@gmail.com>
2025-06-05 13:46:41 +0300

    LoginVC created. Contents connected.

---
+++ Talkind/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  Talkind
+//
+//  Created by Azim Güneş on 5.06.2025.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+
+}
--- Talkind/WelcomeVC.swift
+++ Talkind/WelcomeVC.swift
@@ -12,12 +12,27 @@ import FirebaseCore
 
 class WelcomeVC: UIViewController {
 
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBOutlet weak var toLoginVCButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     
+        toLoginVCButton.layer.cornerRadius = 20
         
     }
 
+    @IBAction func toLoginVC(_ sender: UIButton) {
+        performSegue(withIdentifier: "toLoginVC", sender: self)
 
+    }
+    
 }
 
