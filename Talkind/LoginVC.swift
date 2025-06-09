aedc960ddcc6ceecddd4546bb8630af907718cba
azimgunes <azimgunesofficial@gmail.com>
2025-06-09 18:16:39 +0300

    LoginVC UI problems solved. Elements positioned.

--- Talkind/LoginVC.swift
+++ Talkind/LoginVC.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    @IBOutlet weak var googleButton: UIButton!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signButton: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    
+        Extensions()
     }
  
 
+    
+    @objc func Extensions(){
+        
+        
+        //Buttons
+        
+        googleButton.layer.cornerRadius = 10
+        googleButton.clipsToBounds = true
+        
+        
+        signButton.layer.cornerRadius = 10
+        signButton.clipsToBounds = true
+        
+        
+        //Text Fields
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "E-mail", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        emailTextField.layer.cornerRadius = 15
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        passwordTextField.layer.cornerRadius = 15
+    }
 }
